5be09747de34a60cc6deadacb2565c64b124b8a5
Salomão Nunes <reinaldo.vombo@gmail.com>
2023-04-11 18:21:52 +0100

    set up

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+// import type { MongoClient } from 'mongodb';
+
+declare global {
+  namespace globalThis {
+    var prismadb: PrismaClient;
+    prismadb;
+  }
+}
