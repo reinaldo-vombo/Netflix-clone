ff124db312af194120c876e1f431f90ada0ac88c
Salomão Nunes <reinaldo.vombo@gmail.com>
2023-04-17 17:06:46 +0000

    connect to mongodb

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,24 @@
+// import NextAUTH from "next-auth/next";
+// import Credentials from "next-auth/providers/credentials";
+
+// export default NextAuth({
+//    providers: [
+//       Credentials({
+//          id: 'credential',
+//          name: 'Credentials',
+//          credentials: {
+//             email: {
+//                label: 'Email',
+//                type: 'test'
+//             },
+//             password: {
+//                label: 'password',
+//                type: 'password'
+//             }
+//          },
+//          async autharid(credentials){
+//             if(!credentials)
+//          }
+//       })
+//    ]
+// })
